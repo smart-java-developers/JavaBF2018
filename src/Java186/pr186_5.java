bb7cfea6b319c9a4fb6e68162f369e457828ccb1
mirozspace <zstefchev@gmail.com>
2018-11-09 23:29:13 +0200

    Pr7p168

--- src/Java186/pr186_5.java
+++ src/Java186/pr186_5.java
@@ -6,7 +6,26 @@ public class pr186_5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-
-
+        if (n < 1) {
+            System.out.println("0");
+        } else {
+            int first = 0;
+            int second = 1;
+            int third;
+            System.out.print(first + ", ");
+            System.out.print(second + ", ");
+            for (int i = 2; i < n; i++) {
+                third = first + second;
+                System.out.print(third + ", ");
+                first = second;
+                second = third;
+            }
+        }
     }
 }
+
+
+/*Напишете програма, която чете от конзолата числото N и отпечатва
+сумата на първите N члена от редицата на Фибоначи: 0, 1, 1, 2, 3, 5, 8,
+13, 21, 34, 55, 89, 144, 233, 377, ...
+ */
