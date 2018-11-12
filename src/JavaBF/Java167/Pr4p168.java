600147780cb3487ace8cc70078075b1acb98ffc5
Deianov <deqnow@gmail.com>
2018-11-12 21:04:30 +0100

    Pr4p168

---
+++ src/JavaBF/Java167/Pr4p168.java
@@ -0,0 +1,18 @@
+// v.1 Deianov
+
+package JavaBF.Java167;
+
+import java.util.Scanner;
+
+public class Pr4p168 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Задайте цифра (0-9) : ");
+        int num = Integer.parseInt(scanner.nextLine());
+        System.out.println(numberToName(num));
+    }
+    private static String numberToName (int fromNumber) {
+        String names[] = {"нула", "едно", "две", "три", "четири", "пет", "шест", "седем", "осем", "девет",};
+        return (fromNumber > -1 && fromNumber < 10) ? names[fromNumber] : "грешен вход!";
+    }
+}
