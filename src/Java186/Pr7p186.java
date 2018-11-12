e68996410607427f4a31591b4f6210a3b4711fcc
mirozspace <zstefchev@gmail.com>
2018-11-12 10:53:09 +0200

    ExamPrograms ot podgotvitelnia izpit

---
+++ src/Java186/Pr7p186.java
@@ -0,0 +1,23 @@
+package Java186;
+
+import java.util.Scanner;
+
+public class Pr7p186 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int k = Integer.parseInt(scanner.nextLine());
+        int nk = n - k;
+        for (int i = n - 1; i > 0; i--) {
+            n = n * i;
+        }
+        for (int i = k - 1; i > 0; i--) {
+            k = k * i;
+        }
+        for (int i = nk - 1; i > 0; i--) {
+            nk = nk * i;
+        }
+        System.out.println("Result is: " + (n * k / nk));
+
+    }
+}
