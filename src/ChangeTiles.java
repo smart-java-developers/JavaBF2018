02dff88754345fc7dc24b55d9efce2f8c2fa5e5e
AlexKoleva <32323625+AlexKoleva@users.noreply.github.com>
2018-11-12 19:53:59 +0200

    Add files via upload
    AlexKoleva

---
+++ src/ChangeTiles.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ChangeTiles {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        int nPl=Integer.parseInt(reader.readLine());
+        double lenghtTile=Double.parseDouble(reader.readLine());
+        double widthTile=Double.parseDouble(reader.readLine());
+        int lengthBench=Integer.parseInt(reader.readLine());
+        int widthBench=Integer.parseInt(reader.readLine());
+
+
+        int resultPl=nPl*nPl;
+        double resultTile=lenghtTile*widthTile;
+        int bench=lengthBench*widthBench;
+        double result=(resultPl-bench)/resultTile;
+        double time=result*0.2;
+        System.out.printf("%f%n",result);
+        System.out.println(time);
+
+    }
+}
