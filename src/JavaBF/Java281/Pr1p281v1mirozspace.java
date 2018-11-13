fb9757f397c7dbc0644f4d22ed9fa8d1a6967a80
mirozspace <zstefchev@gmail.com>
2018-11-13 17:08:21 +0200

    New

---
+++ src/JavaBF/Java281/Pr1p281v1mirozspace.java
@@ -0,0 +1,18 @@
+package JavaBF.Java281;
+
+import java.util.Scanner;
+
+public class Pr1p281v1mirozspace {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        printHello();
+
+        scanner.close();
+    }
+
+    private static void printHello (){
+        System.out.println("Hello, Peter!");
+    }
+}
