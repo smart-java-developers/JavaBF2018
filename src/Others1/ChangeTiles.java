287cf392d51a8264b06a5d68279d80db38a4d605
Daniela Pavlova <43967035+danniesmile@users.noreply.github.com>
2018-11-13 19:45:21 +0000

    Just the look with Alt+Ctrl+L and length - the name is edited to be correct

--- src/Others1/ChangeTiles.java
+++ src/Others1/ChangeTiles.java
@@ -6,20 +6,20 @@ import java.io.InputStreamReader;
 
 public class ChangeTiles {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
-        int nPl=Integer.parseInt(reader.readLine());
-        double lenghtTile=Double.parseDouble(reader.readLine());
-        double widthTile=Double.parseDouble(reader.readLine());
-        int lengthBench=Integer.parseInt(reader.readLine());
-        int widthBench=Integer.parseInt(reader.readLine());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int nPl = Integer.parseInt(reader.readLine());
+        double lengthTile = Double.parseDouble(reader.readLine());
+        double widthTile = Double.parseDouble(reader.readLine());
+        int lengthBench = Integer.parseInt(reader.readLine());
+        int widthBench = Integer.parseInt(reader.readLine());
 
 
-        int resultPl=nPl*nPl;
-        double resultTile=lenghtTile*widthTile;
-        int bench=lengthBench*widthBench;
-        double result=(resultPl-bench)/resultTile;
-        double time=result*0.2;
-        System.out.printf("%f%n",result);
+        int resultPl = nPl * nPl;
+        double resultTile = lengthTile * widthTile;
+        int bench = lengthBench * widthBench;
+        double result = (resultPl - bench) / resultTile;
+        double time = result * 0.2;
+        System.out.printf("%f%n", result);
         System.out.println(time);
 
     }
