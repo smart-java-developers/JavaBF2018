be91a1eaf3d377e5d90b7e76d00e4f5a94107e52
mirozspace <zstefchev@gmail.com>
2018-11-14 22:49:23 +0200

    Рекурсийка2.

--- src/Tests/TestsWithRecursionV2mirozspace.java
+++ src/Tests/TestsWithRecursionV2mirozspace.java
@@ -4,13 +4,11 @@ public class TestsWithRecursionV2mirozspace {
     public static void main(String[] args) {
         testRecursin1(5);
     }
-
     private static int testRecursin1(int num) {
         if (num <= 0) {
             return 1;
         } else {
             return num * testRecursin1(num - 1);
-
         }
     }
 }
