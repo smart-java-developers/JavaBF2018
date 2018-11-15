4d13db3c49013d522121ffc7d34bcba97f182ce4
mirozspace <zstefchev@gmail.com>
2018-11-15 20:10:43 +0200

    Грешките са оправени.

--- src/Others1/Bus.java
+++ src/Others1/Bus.java
@@ -6,7 +6,7 @@ public class Bus {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int countPeopleOnDeparture = Integer.parseInt(scanner.nextLine());
+        //int countPeopleOnDeparture = Integer.parseInt(scanner.nextLine());
         int busStops = Integer.parseInt(scanner.nextLine());
 
         int totalPassengers = 0;
@@ -14,7 +14,11 @@ public class Bus {
         for (int i = 1; i <= busStops; i++) {
             int peopleOut = Integer.parseInt(scanner.nextLine());
             int peopleIn = Integer.parseInt(scanner.nextLine());
-            totalPassengers += peopleIn;
+
+            //mirozspace
+            //totalPassengers += peopleIn;
+            totalPassengers = totalPassengers + peopleIn - peopleOut;
+            //==========
 
 
             if (i % 2 != 0) {
@@ -23,10 +27,12 @@ public class Bus {
                 totalPassengers -= 2;
             }
 
-            countPeopleOnDeparture -= peopleOut;
-            countPeopleOnDeparture += totalPassengers;
-
+            //countPeopleOnDeparture -= peopleOut;
+            //countPeopleOnDeparture += totalPassengers;
         }
-        System.out.println(countPeopleOnDeparture);
+        //mirozspace
+        //System.out.println(countPeopleOnDeparture);
+        System.out.println(totalPassengers);
+        //==========
     }
 }
