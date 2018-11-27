8a055efe33fb865b0709c27af6c4ef23bae30bc1
Daniela Pavlova <43967035+danniesmile@users.noreply.github.com>
2018-11-27 14:13:24 +0000

    test

---
+++ src/Others1/dhgsuig.java
@@ -0,0 +1,11 @@
+package Others1;
+
+import java.util.Scanner;
+
+public class dhgsuig {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // iughisbgoafibgsihg
+    }
+}
