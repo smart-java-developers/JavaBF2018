1c9147c924da27276f50c082bf97236d266dc5a3
mirozspace <zstefchev@gmail.com>
2018-12-07 19:17:34 +0100

    Dobavih paketa

---
+++ src/Exam1December/Pr1SchoolSupplies.java
@@ -0,0 +1,19 @@
+package Exam1December;
+
+import java.util.Scanner;
+
+public class Pr1SchoolSupplies {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int packagePens = Integer.parseInt(scanner.nextLine());
+        int packegeMerkets = Integer.parseInt(scanner.nextLine());
+        double cleaningDetergent = Double.parseDouble(scanner.nextLine());
+        int discount = Integer.parseInt(scanner.nextLine());
+
+        double total = packagePens * 5.8 + packegeMerkets * 7.2 + cleaningDetergent * 1.2;
+        total = total - (total * (discount * 1.0 / 100));
+
+        System.out.printf("%.3f", total);
+    }
+}
---
+++ src/Exam1December/Pr1TrekkingEquipment.java
@@ -0,0 +1,18 @@
+package Exam1December;
+
+import java.util.Scanner;
+
+public class Pr1TrekkingEquipment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int mountaineers = Integer.parseInt(scanner.nextLine());
+        int carabiners = Integer.parseInt(scanner.nextLine());
+        int ropes = Integer.parseInt(scanner.nextLine());
+        int pickeys = Integer.parseInt(scanner.nextLine());
+
+        double total = (carabiners * 36 + ropes * 3.6 + pickeys * 19.8) * mountaineers;
+        total = total * 1.2;
+
+        System.out.printf("%.2f", total);
+    }
+}
