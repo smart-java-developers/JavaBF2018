0deb8bfd4216883b8e1c3ac48f5502e7b34dd417
kilera1995 <daniel.takov@abv.bg>
2018-12-30 00:37:59 +0200

    new program

---
+++ src/Others1/Pr6ChristmasDecorationKiller95.java
@@ -0,0 +1,34 @@
+package Others1;
+
+import java.util.Scanner;
+
+public class Pr6ChristmasDecorationKiller95 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double budjet = Integer.parseInt(scanner.nextLine());
+        double price;
+        String nameObject = "";
+
+        while (!nameObject.equals("Stop")) {
+            nameObject = scanner.nextLine();
+            if (nameObject.equals("Stop")) {
+                System.out.printf("Money left: %.0f", budjet);
+                return;
+            }
+            price = 0;
+            for (int i = 0; i < nameObject.length(); i++) {
+                price = price + nameObject.charAt(i);
+            }
+            if (price > budjet) {
+                System.out.println("Not enough money!");
+                return;
+            }
+            if (price <= budjet) {
+                System.out.println("Item successfully purchased!");
+            }
+            budjet = budjet - price;
+        }
+    }
+}
+
+
