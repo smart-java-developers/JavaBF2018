0b29b63352904298667a6b76885bf5833a7ee39b
mirozspace <zstefchev@gmail.com>
2019-01-02 22:29:57 +0100

    malka promiana

--- src/KalendatNaBydeshteto/Main.java
+++ src/KalendatNaBydeshteto/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int day = Integer.parseInt(scanner.nextLine());
+        int month = Integer.parseInt(scanner.nextLine());
+        int year = Integer.parseInt(scanner.nextLine());
+        String dayStr = scanner.nextLine();
     }
 }
 
